77b3c914edcf92a09f215d92d68543600ce2e7d0
Vinayak <ebix@Ebixs-MacBook-Pro.local>
2023-02-19 22:49:12 +0530

    added dom url crawler

--- crawl.js
+++ crawl.js
@@ -1,3 +1,31 @@
+const { JSDOM } = require('jsdom')
+
+function getURLfromHTML(htmlBody, baseURL){
+    const urls = []
+    const dom = new JSDOM(htmlBody)
+    const linkElements = dom.window.document.querySelectorAll('a')
+    for(const item of linkElements){
+        if(item.href.slice(0,1) === '/'){
+            //relative
+            try{
+                const urlObj = new URL(`${baseURL}${item.href}`)
+                urls.push(`${baseURL}${item.href}`)
+            }catch(err){
+                console.log(`Error with URL : ${err}`)
+            }   
+        }else{
+            //absolute
+            try{
+                const urlObj = new URL(`${item.href}`)
+                urls.push(item.href)
+            }catch(err){
+                console.log(`Error with URL : ${err}`)
+            }   
+        }
+    }
+    return urls
+}
+
 function normalizeURL(urlString){
     const urlObj = new URL(urlString)
     const hostPath = `${urlObj.hostname}${urlObj.pathname}`;
@@ -8,5 +36,6 @@ function normalizeURL(urlString){
 }
 
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLfromHTML
 } 
--- crawl.test.js
+++ crawl.test.js
@@ -1,21 +1,108 @@
-const { normalizeURL } = require('./crawl.js')
+const { normalizeURL, getURLfromHTML } = require('./crawl.js')
 const { test, expect } = require('@jest/globals')
 
-test('normalizeURL TrailingSlashes', () =>{
+test('normalizeURL TrailingSlashes', () => {
     const url = 'https://blog.boot.dev/path/'
     const actual = normalizeURL(url)
     const expected = 'blog.boot.dev/path'
     expect(actual).toEqual(expected)
 })
-test('normalizeURL UpperCase', () =>{
+test('normalizeURL UpperCase', () => {
     const url = 'https://BLOG.boot.dev/path'
     const actual = normalizeURL(url)
     const expected = 'blog.boot.dev/path'
     expect(actual).toEqual(expected)
 })
-test('normalizeURL strip http', () =>{
+test('normalizeURL strip http', () => {
     const url = 'https://blog.boot.dev/path'
     const actual = normalizeURL(url)
     const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+
+test('getURLfromHTML', () => {
+    const html = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=la, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <a href="https://blog.boot.dev/">
+            boot.dev Blog
+        </a>
+    </body>
+    </html>`
+    const baseURL = 'https://blog.boot.dev'
+    const actual = getURLfromHTML(html, baseURL)
+    const expected = ['https://blog.boot.dev/']
+    expect(actual).toEqual(expected)
+})
+
+test('getURLfromHTML relative', () => {
+    const html = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=la, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <a href="/path/">
+            boot.dev Blog
+        </a>
+    </body>
+    </html>`
+    const baseURL = 'https://blog.boot.dev'
+    const actual = getURLfromHTML(html, baseURL)
+    const expected = ['https://blog.boot.dev/path/']
+    expect(actual).toEqual(expected)
+})
+
+test('getURLfromHTML bothRelativeandAbsolute', () => {
+    const html = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=la, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <a href="https://blog.boot.dev/">
+            boot.dev Blog
+        </a>
+        <a href="/path/">
+        boot.dev Blog
+    </a>
+    </body>
+    </html>`
+    const baseURL = 'https://blog.boot.dev'
+    const actual = getURLfromHTML(html, baseURL)
+    const expected = ['https://blog.boot.dev/', 'https://blog.boot.dev/path/']
+    expect(actual).toEqual(expected)
+})
+
+test('getURLfromHTML invalidURL', () => {
+    const html = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=la, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <a href="invalid">
+        boot.dev Blog
+        </a>
+    </body>
+    </html>`
+    const baseURL = 'https://blog.boot.dev'
+    const actual = getURLfromHTML(html, baseURL)
+    const expected = []
     expect(actual).toEqual(expected)
 })
